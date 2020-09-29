90408cb6728269f5916698cb9a1cb48a600a9197
3madov-77 <emadzxy7@gmail.com>
2020-09-29 03:42:51 +0300

    finshed

--- books_api/settings.py
+++ books_api/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'books_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
@@ -123,6 +127,6 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASS': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ]
 }
