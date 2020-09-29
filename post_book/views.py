523492e53ba3c75a759403fab6eb575e123cdc6a
3madov-77 <emadzxy7@gmail.com>
2020-09-29 03:43:12 +0300

    finshed

--- post_book/views.py
+++ post_book/views.py
@@ -4,11 +4,13 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import BookSerializer
 from .models import Book
+from .permissions import ReadOnly
 
 class PostBook(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class RetrieveBook(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (ReadOnly,)
     queryset = Book.objects.all()
     serializer_class = BookSerializer
